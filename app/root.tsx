a51668461b99f6149f1cee0e5e4d53881cebc6ab
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2025-04-11 19:45:56 +0330

    Add the base property

--- app/root.tsx
+++ app/root.tsx
@@ -21,8 +21,8 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "icon", href: "/favicon.ico", type: "image/ico"},
-  { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+  { rel: "icon", href: "~/assets/favicon.ico", type: "image/ico"},
+  { rel: "apple-touch-icon", href: "~/assets/apple-touch-icon.png" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
